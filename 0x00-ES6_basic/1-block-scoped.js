fd62283f3448b8ce81521c29bcdda1858e805ce8
doloadama <doloadama@outlook.com>
2023-01-25 08:43:30 +0000

    .

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-    let task = false;
-    let task2 = true;
+    var task = false;
+    var task2 = true;
   
     if (trueOrFalse) {
-      var task = true;
-      var task2 = false;
+      let task = true;
+      let task2 = false;
     }
   
     return [task, task2];
