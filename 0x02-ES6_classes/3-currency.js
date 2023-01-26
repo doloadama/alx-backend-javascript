552df014432797986a2393600ad706cb9653ab9d
doloadama <doloadama@outlook.com>
2023-01-26 07:13:33 +0000

    2-getters and setters

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,33 @@
+export class HolbertonCourse{
+
+    constructor(name, lenght, students){
+        if (typeof name !== 'string' || typeof lenght !== "number" || typeof students !== "string") throw new Error();
+        this._name = name;
+        this._lenght = lenght;
+        this._students = students;
+    }
+
+    get name(){
+        return this._name;
+    }
+
+    set name(val){
+        this._name = val;
+    }
+
+    get lenght(){
+        return this._lenght;
+    }
+
+    set lenght(val){
+        this._lenght = val;
+    }
+
+    get students(){
+        return this._students;
+    }
+
+    set students(val){
+        this._students = val;
+    }
+}
