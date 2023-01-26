0e7485345d6fdd741936859d60401b01f29cdcd3
doloadama <doloadama@outlook.com>
2023-01-26 22:14:57 +0000

    .

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,31 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set size(val) {
+    this._size = val;
+  }
+
+  set location(val) {
+    this._location = val;
+  }
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'string') {
+      return this._location;
+    }
+    if (hint === 'number') {
+      return this._size;
+    }
+    return this;
+  }
+}
