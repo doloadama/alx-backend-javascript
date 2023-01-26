7db45e4bc27392cc5c7d08859a81bbf709786edb
doloadama <doloadama@outlook.com>
2023-01-26 20:51:20 +0000

    .

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,28 @@
+import Building from './5-building.js'
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._sqft = sqft;
+    this._floors = floors;
+  }
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(val) {
+    this._sqft = val;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(val) {
+    this._floors = val;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
