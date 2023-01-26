7082c42e70958caca5c8ee117ec1725b97f996ff
doloadama <doloadama@outlook.com>
2023-01-26 10:51:29 +0000

    .

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,5 +1,4 @@
-import Classroom from "./0-classroom";
-
-   export default function initializeRooms(){
-        return [19, 20, 34].map((size) => new Classroom(size))
+import Classroom from './0-classroom';
+export default function initializeRooms() {
+  return [19, 20, 34].map((size) => new Classroom(size))
 }
