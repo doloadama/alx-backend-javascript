8c16fe8bc8c236b352bc1f31142428c4d290937c
doloadama <doloadama@outlook.com>
2023-01-26 22:01:20 +0000

    .

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,25 @@
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+  get name() {
+    return this._name;
+  }
+
+  set name(val) {
+    this._name = val;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(val) {
+    this._code = val;
+  }
+
+  get [Symbol.toStringTag]() {
+    return this.code;
+  }
+}
