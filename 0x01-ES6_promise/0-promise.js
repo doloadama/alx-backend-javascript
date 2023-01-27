ed0bad1d79ef8c1a3b76fa4bc0727e92534306d1
doloadama <doloadama@outlook.com>
2023-01-27 22:08:45 +0000

    .

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,13 +1,3 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({
-        status: 200,
-        body: 'Success',
-      });
-    } 
-    else {
-      reject(new Error('The fake API is not working currently'));
-    }
-  });
+  return new Promise(() => {});
 }
---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,13 @@
+export default function getResponseFromAPI(Success) {
+  return new Promise((resolve, reject) => {
+    if (Success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } 
+    else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
