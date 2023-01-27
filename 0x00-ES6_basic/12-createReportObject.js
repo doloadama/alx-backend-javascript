dd508d2293b76c8cd15011bbdb6c271ccebc5125
doloadama <doloadama@outlook.com>
2023-01-27 16:58:06 +0000

    .

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,7 +1,7 @@
 export default function createReportObject(employeesList) {
   return {
-    allEmployees: { ...employeesList },
-    getNumberofDepartements(employeesList) {
+    allEmployees: employeesList,
+    getNumberOfDepartments(employeesList) {
       return Object.keys(employeesList).length;
     },
   };
