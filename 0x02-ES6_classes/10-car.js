4a3aeb451c457277a7f03c01a832e187afef84f5
doloadama <doloadama@outlook.com>
2023-01-27 00:33:29 +0000

    .

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -5,6 +5,34 @@ export default class Car {
     this._color = color;
   }
 
+  get brand() {
+    return this._brand;
+  }
+
+  set brand(val) {
+    this._brand = val;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  set motor(val) {
+    this._motor = val;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set color(val) {
+    this._color = val;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
   cloneCar() {
     const Nouveau = this.constructor[Symbol.species];
     return new Nouveau();
