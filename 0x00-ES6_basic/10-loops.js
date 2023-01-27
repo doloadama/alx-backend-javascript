2ed9c9ac20467939f09266c400e318e2ce017a5a
doloadama <doloadama@outlook.com>
2023-01-27 11:32:52 +0000

    .

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -2,8 +2,7 @@ export default function appendToEachArrayValue(array, appendString) {
   let idx;
   const newA = [];
   for (idx of array) {
-    newA.push(appendString + idx)
+    newA.push(appendString + idx);
   }
   return array;
-  }
-  
+}
