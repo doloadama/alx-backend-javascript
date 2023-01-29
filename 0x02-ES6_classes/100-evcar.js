e746020a2217a918954b8bf61f41dc55f060650c
doloadama <doloadama@outlook.com>
2023-01-29 19:11:34 +0000

    .

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,19 @@
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+   super(brand, motor, color);
+    this._range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  set range(val) {
+    this._range = val;
+  }
+  CloneCar() {
+    return new Car(this.brand, this.motor, this.color);
+  }
+}
