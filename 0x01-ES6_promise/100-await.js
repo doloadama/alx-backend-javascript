1b013edd432c101b8816bcd05df37f338874a2fa
doloadama <doloadama@outlook.com>
2023-01-29 16:38:52 +0000

    .

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,15 @@
+import {uploadPhoto, createUser} from './utils';
+
+export default async function asyncUploadUser() {
+  const reponse = {
+    photo: null,
+    user: null
+  };
+  try {
+    reponse.photo = await uploadPhoto();
+    reponse.user = await createUser();
+  } catch (error) {
+    return reponse;
+  }
+  return reponse;
+}
