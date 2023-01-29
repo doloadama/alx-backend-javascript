76adf9d8f62b392c7cf4961359be7caa4bd4948b
doloadama <doloadama@outlook.com>
2023-01-29 10:19:28 +0000

    .

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -6,17 +6,7 @@ function guardrail(mathFunction) {
   } catch (error) {
     queue.push(error.message);
   } finally {
-<<<<<<< HEAD
-<<<<<<< HEAD
     queue.push('Guardrail was processed');
-
-    return queue;
-=======
-    queue.push("Guardrail was processed");
->>>>>>> 0a9dd3abee3d5f517baa69fcdc5af14c6a940eff
-=======
-    queue.push('Guardrail was processed');
->>>>>>> 2faa4e3db5ca9b6634f7e5f17c29676e8fef441e
   }
   return queue;
 }
