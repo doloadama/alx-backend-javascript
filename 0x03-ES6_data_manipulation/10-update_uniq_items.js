915ad49f85dcb761a649d6dac3ad4c5c392eb114
doloadama <doloadama@outlook.com>
2023-01-30 21:06:32 +0000

    .

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,12 +1,10 @@
-import groceriesList from './9-groceries_list';
-
 export default function updateUniqueItems(map) {
-  const updateUniqueItems = map => {
-    if (!map) throw new Error("Cannot process");
-    for (const [key, value] of map) {
-      if (value === 1) {
-        map.set(key, 100);
-      }
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  map.forEach((value, key) => {
+    if (value === 1) {
+      map.set(key, 100);
     }
-  };
+  });
 }
