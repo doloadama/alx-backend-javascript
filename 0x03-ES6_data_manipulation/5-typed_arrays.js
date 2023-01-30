61b67a637cb9020eb3d8f9e84420728200ff029f
doloadama <doloadama@outlook.com>
2023-01-30 15:58:01 +0000

    .

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,8 +1,8 @@
 export default function createInt8TypedArray(lenght, position, value) {
-  
-
-
-
-
-
+  if (position => lenght) throw Error('Position outside range')  
+  const buffer = new ArrayBuffer(lenght);
+  const newView = new DataView(buffer, 0, lenght);
+  const arr = new Int8Array(buffer);
+  arr[position] = value;
+  return newView;
 }
