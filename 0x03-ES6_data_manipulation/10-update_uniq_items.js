ef5a741477b47ccc0d7beb033c9def17cb917405
doloadama <doloadama@outlook.com>
2023-01-30 20:55:24 +0000

    .

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,6 @@
+export default function updateUniqueItems(map) {
+ if (!(map instanceof Map)) throw new Error('Cannot process');
+ map.forEach((Val, key) => {
+  if (val === 1) map.set(key, 100);
+ });
+}
