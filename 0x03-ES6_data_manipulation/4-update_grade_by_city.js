41e8f9aedf5c5a77e80745dacdefe3de9f41d380
doloadama <doloadama@outlook.com>
2023-01-30 10:00:47 +0000

    .

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,9 +1,12 @@
-export default function updateStudentGradeByCity(student , city, newGrades) {
-  return student
-    .filter(student => student.location === city)
-    .map(student => {
-      let studentGrade = newGrades.find(grade => grade.studentId === student.id);
-      student.grade = studentGrade ? studentGrade.grade : "N/A";
-      return student;
+export default function updateStudentGradeByCity(arr, city, newGrades) {
+  return arr
+    .filter((el) => el.location === city)
+    .map((person) => {
+      const grades = newGrades.filter((el) => el.studentId === person.id);
+      const grade = grades.length ? grades[0].grade : 'N/A';
+      return {
+        ...person,
+        grade,
+      };
     });
 }
