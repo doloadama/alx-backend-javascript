926e44c4555ff518c5ef44b510c831335713d343
doloadama <doloadama@outlook.com>
2023-01-30 10:03:02 +0000

    .

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,10 +1,11 @@
 import getListStudents from './0-get_list_students';
-export default function updateStudentGradeByCity (student = getListStudents, city, newGrades) {
+
+export default function updateStudentGradeByCity(student = getListStudents, city, newGrades) {
   return student
-    .filter(student => student.location === city)
-    .map(student => {
-      let studentGrade = newGrades.find(grade => grade.studentId === student.id);
-      student.grade = studentGrade ? studentGrade.grade : "N/A";
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const studentGrade = newGrades.find((grade) => grade.studentId === student.id);
+      student.grade = studentGrade ? studentGrade.grade : 'N/A';
       return student;
     });
 }
