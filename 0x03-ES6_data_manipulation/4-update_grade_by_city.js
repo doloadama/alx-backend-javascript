bf757cf1756edf718a567f852c69f9e5a0509f6f
doloadama <doloadama@outlook.com>
2023-01-30 09:07:12 +0000

    .

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,5 +1,4 @@
-import getListStudents from './0-get_list_students';
-export default function updateStudentGradeByCity (student = getListStudents, city, newGrades) {
+export default function updateStudentGradeByCity(student , city, newGrades) {
   return student
     .filter(student => student.location === city)
     .map(student => {
