46a5a1d1e20042938886f34e9ae1ee434a562d3f
doloadama <doloadama@outlook.com>
2023-01-30 10:08:57 +0000

    .

--- 0x03-ES6_data_manipulation/1-main.js
+++ 0x03-ES6_data_manipulation/1-main.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-import getListStudentIds from './1-get_list_student_ids.js';
-=======
 import getListStudentIds from './1-get_list_student_ids';
->>>>>>> 4c72c3f1feddb390d6dc54a19f9c79e7cf227e9f
-import getListStudents from './0-get_list_students.js';
+import getListStudents from './0-get_list_students';
 
 console.log(getListStudentIds('hello'));
 console.log(getListStudentIds(getListStudents()));
--- 0x03-ES6_data_manipulation/2-main.js
+++ 0x03-ES6_data_manipulation/2-main.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-import getListStudents from './0-get_list_students.js';
-import getStudentsByLocation from './2-get_students_by_loc.js';
-=======
 import getListStudents from './0-get_list_students';
 import getStudentsByLocation from './2-get_students_by_loc';
->>>>>>> 4c72c3f1feddb390d6dc54a19f9c79e7cf227e9f
 
 const students = getListStudents();
 
