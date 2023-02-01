5781a56435a5212f29d30df406b6fd2fe0bf18d5
doloadama <doloadama@outlook.com>
2023-02-01 18:56:06 +0000

    .

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,77 @@
+// DirectorInterface
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+// TeacherInterface
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+// Director class
+export class Director implements DirectorInterface {
+  workFromHome(): string {
+    return "Working from home";
+  }
+  getCoffeeBreak(): string {
+    return "Getting a coffee break";
+  }
+  workDirectorTasks(): string {
+    return "Getting to director tasks";
+  }
+}
+
+// Teacher class
+export class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return "Cannot work from home";
+  }
+  getCoffeeBreak(): string {
+    return "Cannot have a break";
+  }
+  workTeacherTasks(): string {
+    return "Getting to work";
+  }
+}
+
+// createEmployee function
+function createEmployee(salary: number | string): Director | Teacher {
+  if (typeof salary === "number" && salary < 500) {
+    return new Teacher();
+  } else {
+    return new Director();
+  }
+}
+
+// isDirector function
+export function isDirector(employee: TeacherInterface | DirectorInterface): employee is Director {
+  return (employee as Director).workDirectorTasks() !== undefined;
+}
+
+// executeWork function
+export function executeWork(employee: TeacherInterface | DirectorInterface): string {
+  if (isDirector(employee)) {
+    return (employee as Director).workDirectorTasks();
+  } else {
+    return (employee as Teacher).workTeacherTasks();
+  }
+}
+
+type Subjects = 'Math' | 'History';
+
+export function teachClass(todayClass: Subjects): string | boolean{
+  return (todayClass === 'Math') ? 'Teaching Math' : (todayClass === 'History') ? 'Teaching History': ``;
+}
+
+
+
+
+
+
+
+
+
---
+++ 0x04-TypeScript/task_3/js/interface.ts
@@ -0,0 +1,9 @@
+//type RowID that is a number
+type RowID = number;
+
+// interface that contains 3 fiels
+interface RowElement {
+  firstName: string;
+  lastName: string;
+  age: number;
+}
