bcd4c66e53eb2d96e56fe60ae04284bf63beb54e
doloadama <doloadama@outlook.com>
2023-03-22 11:23:32 +0000

    module.export added

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,5 +1,6 @@
-export function calculateNumber(a, b) {
+function calculateNumber(a, b) {
     let roundedA = Math.round(a);
     let roundedB = Math.round(b);
     return roundedA + roundedB;
 }
+module.exports = calculateNumber;
