b4c3c87725fe603c12758cf711094994be932ad6
doloadama <doloadama@outlook.com>
2023-03-22 11:43:54 +0000

    upgrade of the previous files by addind a type

---
+++ 0x06-unittests_in_js/1-calcul.js
@@ -0,0 +1,18 @@
+function calculateNumber(type, a, b) {
+    let roundedA = Math.round(a);
+    let roundedB = Math.round(b);
+    switch (type) {
+        case "SUM":
+            return roundedA + roundedB;
+        case "SUBSTRACT":
+            return roundedA - roundedB;
+        case "DIVIDE":
+            if (roundedB === 0) {
+                return Error
+            }
+            return roundedA / roundedB;
+        default:
+            return "Invalid type";
+    }
+}
+module.exports = calculateNumber;
