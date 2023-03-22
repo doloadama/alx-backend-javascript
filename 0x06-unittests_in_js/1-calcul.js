c775d6f6d9e5d0e8adc96f10d720afbeb7bd2250
doloadama <doloadama@outlook.com>
2023-03-22 11:51:26 +0000

    Subtract modified

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -5,6 +5,8 @@ function calculateNumber(type, a, b) {
         case "SUM":
             return roundedA + roundedB;
         case "SUBSTRACT":
+            if (roundedB < roundedA)
+                return Error;
             return roundedB - roundedA;
         case "DIVIDE":
             if (roundedB === 0) {
