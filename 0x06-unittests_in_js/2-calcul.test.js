3bbe939d417469b20818becc5397c2b4f1b3d092
doloadama <doloadama@outlook.com>
2023-03-22 17:34:19 +0000

    A new test case using express from chai

---
+++ 0x06-unittests_in_js/2-calcul.test.js
@@ -0,0 +1,26 @@
+const expect = require('chai').expect;
+const calculateNumber = require('./2-calcul.js');
+
+describe('calculateNumber', function() {
+  describe('SUM operation', function() {
+    it('should return the sum of two integers', function() {
+      expect(calculateNumber('SUM', 1.4, 4.5), 6);
+    });
+  });
+
+  describe('SUBTRACT operation', function() {
+    it('should return the difference of two integers', function() {
+      expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+    });
+  });
+
+  describe('DIVIDE operation', function() {
+    it('should return the quotient of two integers', function() {
+      expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+    });
+
+    it('should return "Error" if the second number is 0', function() {
+      expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+    });
+  });
+});
