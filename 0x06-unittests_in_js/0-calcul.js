146080a6b5af1c1fddb838eef23b2f775a8e028c
doloadama <doloadama@outlook.com>
2023-03-22 11:13:28 +0000

    deleted export module

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,4 +1,4 @@
-export default function calculateNumber(a, b) {
+function calculateNumber(a, b) {
     const roundedA = Math.round(a);
     const roundedB = Math.round(b);
     return roundedA + roundedB;
