2f9792f0919aa285c4b06ab8f00b11682f9be584
doloadama <doloadama@outlook.com>
2023-03-22 11:06:03 +0000

    add of new tests cases

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,10 +1,27 @@
+// Import the assert module
 const assert = require('assert');
-const calculateNumber = require('./calculateNumber');
 
-describe('calculateNumber', () => {
-  it('returns the sum of two rounded numbers', () => {
-    assert.equal(calculateNumber(3.7, 2.2), 6);
-    assert.equal(calculateNumber(2.4, 3.6), 6);
-    assert.equal(calculateNumber(-3.7, -2.2), -6);
+// Import the function to test
+const calculateNumber = require('./0-calcul.js');
+
+// Describe the test suite
+describe('calculateNumber', function() {
+  // Test normal cases with different combinations of integers and floats
+  it('should return the sum of two numbers rounded to the nearest integer', function() {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    assert.strictEqual(calculateNumber(-1, -3), -4);
+    assert.strictEqual(calculateNumber(-1.4, -3.6), -5);
+    assert.strictEqual(calculateNumber(-1.6, -3.6), -6);
+  });
+
+  // Test edge cases with zero and infinity
+  it('should handle zero and infinity', function() {
+    assert.strictEqual(calculateNumber(0, 0), 0);
+    assert.strictEqual(calculateNumber(0, Infinity), Infinity);
+    assert.strictEqual(calculateNumber(Infinity, Infinity), Infinity);
+    assert.strictEqual(isNaN(calculateNumber(NaN, NaN)), true); // NaN is not equal to anything
   });
 });
