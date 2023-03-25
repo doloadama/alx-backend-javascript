6edce38e331e34567173b6dd9ade6cca7b74b2a7
doloadama <doloadama@outlook.com>
2023-03-25 10:31:37 +0000

    error in Api.js

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,3 +1,16 @@
+const net = require('net');
+
+const server = net.createServer();
+server.once('error', (err) => {
+  if (err.code === 'EADDRINUSE') {
+    console.error('Port 7865 is already in use');
+  }
+});
+
+server.listen(7865, () => {
+  console.log('API available on localhost port 7865');
+});
+
 const express = require('express');
 
 const app = express();
