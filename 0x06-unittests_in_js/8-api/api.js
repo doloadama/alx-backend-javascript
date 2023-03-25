2625723feb0c5a5ad098800bbe635a8818e273b2
doloadama <doloadama@outlook.com>
2023-03-25 10:38:04 +0000

    error keep popping

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,28 +1,14 @@
-const net = require('net');
-
-const server = net.createServer();
-server.once('error', (err) => {
-  if (err.code === 'EADDRINUSE') {
-    console.error('Port 7865 is already in use');
-  }
-});
-
-server.listen(7865, () => {
-  console.log('API available on localhost port 7865');
-});
-
 const express = require('express');
 
 const app = express();
-
-const port = 7865;
+const PORT = 7865;
 
 app.get('/', (_, res) => {
   res.send('Welcome to the payment system');
 });
 
-app.listen(port, () => {
-  console.log(`API available on localhost port ${port}`);
+app.listen(PORT, () => {
+  console.log(`API available on localhost port ${PORT}`);
 });
 
 module.exports = app;
