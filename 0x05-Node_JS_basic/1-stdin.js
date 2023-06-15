e6ea50b4024a32cae36ab577232d2a2e4fd57f12
doloadama <doloadama@outlook.com>
2023-06-15 10:13:01 +0000

    .

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,25 +4,11 @@ process.stdin.setEncoding('utf8');
 console.log('Welcome to Holberton School, what is your name?');
 
 // Listen for data on STDIN
-process.stdin.on('data', (data) => {
+process.stdin.once('data', (data) => {
   // Process the input
   const input = data.trim(); // Remove trailing newline character
 
-  // Check if the program is ending
-  if (input.toLowerCase() === 'exit') {
-    console.log('This important software is now closing');
-    process.exit(0); // Exit the program with success status code
-  } else {
-    // Display the user's name
-    console.log(`Your name is: ${input}`);
-  }
-});
-
-// Listen for 'end' event on STDIN
-process.stdin.on('end', () => {
+  // Display the user's name
+  console.log(`Your name is: ${input}`);
   console.log('This important software is now closing');
-  process.exit(0); // Exit the program with success status code
 });
-
-// Resume the STDIN stream
-process.stdin.resume();
