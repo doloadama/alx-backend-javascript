8742aede915f66e100077ba3f0516239fa67b64a
doloadama <doloadama@outlook.com>
2023-06-16 21:06:08 +0000

    .

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -7,8 +7,10 @@ describe('sendPaymentRequestToApi', () => {
   describe('calculateNumber', () => {
     it('should call the calculateNumber function from Utils', () => {
       const calculateNumberSpy = sinon.spy(Utils);
+
       sendPaymentRequestToApi(100, 20);
       expect(calculateNumberSpy.calledWith('SUM', 100, 20)).to.be.true;
+      expect(calculateNumberSpy.callCount).to.be.equal(1);
       calculateNumberSpy.restore();
     });
   });
