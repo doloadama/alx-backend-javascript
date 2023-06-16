e81ceb6a9b62c5698894f3094ed68733c3ca200f
doloadama <doloadama@outlook.com>
2023-06-16 21:39:54 +0000

    Promises

---
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -0,0 +1,10 @@
+function getPaymentTokenFromAPI(success) {
+    if (success) {
+      return Promise.resolve({ data: 'Successful response from the API' });
+    } else {
+      return Promise.resolve();
+    }
+  }
+  
+  module.exports = getPaymentTokenFromAPI;
+  
---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,12 @@
+const getPaymentTokenFromAPI = require('./6-payment_token.js');
+
+describe('getPaymentTokenFromAPI', () => {
+  it('should return a resolved promise with the correct data when success is true', (done) => {
+    getPaymentTokenFromAPI(true)
+      .then((result) => {
+        expect(result).to.deep.equal({ data: 'Successful response from the API' });
+        done();
+      })
+      .catch(done);
+  });
+});
