2b079e5c697cd83f5b4f653b1df51054200c518a
doloadama <doloadama@outlook.com>
2023-06-16 21:12:27 +0000

    .

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,6 +1,8 @@
 const Utils = require('./utils')
 
-function sendPaymentRequestToApi(totalAmount, totalShipping) {
+const sendPaymentRequestToApi = (totalAmount, totalShipping) {
     const sum = Utils.calculateNumber('SUM', totalAmount, totalShipping);
     console.log(`The total is: ${sum}`)
 };
+
+module.exports = sendPaymentRequestToApi;
