03b37d3556771a9d5ece4e2bb19d75062c766a52
doloadama <doloadama@outlook.com>
2023-06-16 09:46:40 +0000

    .

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,7 +1,5 @@
-// 0-calcul.js
-
 function calculateNumber(a, b) {
-  const roundedA = Math.round(a);
-  const roundedB = Math.round(b);
-  return roundedA + roundedB;
+  return Math.round(a) + Math.round(b);
 }
+
+module.exports = calculateNumber;
