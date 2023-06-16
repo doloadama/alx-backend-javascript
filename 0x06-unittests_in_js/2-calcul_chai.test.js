3f787705384fa01c339a2aff59c84f28d5a65476
doloadama <doloadama@outlook.com>
2023-06-16 11:06:22 +0000

    .

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -4,14 +4,14 @@ const { expect } = require("chai");
 describe('calculateNumber', () => {
   describe('when type is SUM', () => {
     it('should return the sum of rounded numbers', () => {
-      expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(4);
+      expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
       expect(calculateNumber('SUM', 1, 3.7)).to.equal(5);
     });
   });
 
   describe('when type is SUBTRACT', () => {
     it('should return the subtraction of rounded numbers', () => {
-      expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-3);
+      expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
       expect(calculateNumber('SUBTRACT', 1, 3.7)).to.equal(-3);
       expect(calculateNumber('SUBTRACT', 1.2, 3.7)).to.equal(-3);
       expect(calculateNumber('SUBTRACT', 1.5, 3.7)).to.equal(-2);
