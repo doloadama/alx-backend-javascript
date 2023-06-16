219bd8092c24b4a30135d3cebf79b8c9d279686a
doloadama <doloadama@outlook.com>
2023-06-16 10:04:19 +0000

    .

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -5,7 +5,7 @@ describe('calculateNumber', () => {
   it('should return the sum of rounded numbers', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
     assert.strictEqual(calculateNumber(1, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 4);
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 });
