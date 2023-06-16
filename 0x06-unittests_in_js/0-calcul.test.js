d9c623f3178bae07fb63aa6b87c18dd8954438ab
doloadama <doloadama@outlook.com>
2023-06-16 10:17:07 +0000

    .

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,9 +4,21 @@ const calculateNumber = require('./0-calcul');
 describe('calculateNumber', () => {
   it('should return the sum of rounded numbers', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+
+  it('should return rounded second number', () => {
     assert.strictEqual(calculateNumber(1.4, 3.6), 5);
+  });
+
+  it('should return rounded first and second numbers', () => {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+
+  it('should return rounded first and second numbers', () => {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+
+  it('rounding down b\'s floating point fractional number', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.4), 3);
   });
 });
