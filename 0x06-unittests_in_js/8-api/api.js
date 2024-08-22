a3705fd1d9a45a29b7c5af2654dce1400afbb272
doloadama <98645172+doloadama@users.noreply.github.com>
2024-08-22 09:44:18 +0000

    Update api.js

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -11,4 +11,10 @@ app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
 
+app.get('/cart/:id([0-9]+)', (req, res) => {
+    const cartId = req.params.id;
+    res.send(`Payment methods for cart ${cartId}`);
+});
+
+
 module.exports = app;
